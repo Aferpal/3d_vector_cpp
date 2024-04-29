86e719c481f316420337ce7606de7f8d9f1605b7
Aferpal <fernandezpalomoabel@gmail.com>
2024-04-29 22:57:52 +0200

    Adding first Point3D class

---
+++ src/basicClasses/Point.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Point3D{
+private:
+	float x, y, z;
+public:
+	Point3D(float, float, float);
+	Point3D(const Point3D&);
+	Point3D(Point3D&&);
+	float getX()const;
+	float getY()const;
+	float getZ()const;
+	void setX(float);
+	void setY(float);
+	void setZ(float);
+
+};
